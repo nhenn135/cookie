461b3529f32207ff68acd2dadfa9772a74991a8b
Milanotruffles <118767472+Milanotruffles@users.noreply.github.com>
2023-01-18 12:23:36 -0600

    Another test complie

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import string
-from random import *
-
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
-chars = letters + digits + symbols
-
-min_length = 12
-max_length = 12
-password = "".join(choice(chars) for x in range(randint(min_length, max_length)))
-print(password)
+print("Hello world")
