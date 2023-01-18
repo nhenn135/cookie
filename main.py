b630afa4f473ce24a0cdbe903c6b1023874fb660
Milanotruffles <118767472+Milanotruffles@users.noreply.github.com>
2023-01-18 12:26:42 -0600

    Password generator
    12 character pull no spaces

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import string
+from random import *
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+chars = letters + digits + symbols
+
+min_length = 12
+max_length = 12
+password = "".join(choice(chars) for x in range(randint(min_length, max_length)))
+print(password)
