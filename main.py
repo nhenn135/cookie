1cdc83cf9f8db155a03bb99e0eeec3cf9d180a28
Milanotruffles <118767472+Milanotruffles@users.noreply.github.com>
2023-01-18 12:27:55 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import random
+
+# creates 5 unique elements from 1..50 and adds a + and a [0-19]+1 number
+randlist = random.sample(range(1,54),k=6) + ["+", random.choice(range(26))+1]
+print(randlist)
+
+print("You drew {} {} {} {} {} {} {}".format(*randlist))
