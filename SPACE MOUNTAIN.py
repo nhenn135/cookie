d47d867410a00c601f30e9420d3282ff795e07fd
Nick <118767472+nhenn135@users.noreply.github.com>
2023-02-22 15:11:28 -0600

    Add files via upload

---
+++ SPACE MOUNTAIN.py
@@ -0,0 +1,7 @@
+height = input("How tall are youm in centimeters")
+height = int(height)
+
+if height >= 122:
+    print("\nYour tall enough to ride space mountain!")
+else:
+    print("\n I'm sorry little one, you are not tall enough to ride space mountain safely.")
